4500809353327ea24524a9384db6067e0f636c1c
AndaMat003 <120305928+AndaMat003@users.noreply.github.com>
2025-05-11 21:07:02 +0200

    I did Delivery and Manufacture

---
+++ src/main/java/za/ac/cput/domain/Manufacture.java
@@ -0,0 +1,123 @@
+package za.ac.cput.domain;
+
+import java.util.List;
+
+public class Manufacture {
+    private final int manufactureNumber;
+    private final String manufactureName;
+    private final String inventoryStock;
+    private final ContactDetails contactDetails;
+    private final List<Candle> candles;
+    private final List<Delivery> deliveries;
+
+    private Manufacture() {
+        this.manufactureNumber = 0;
+        this.manufactureName = null;
+        this.inventoryStock = null;
+        this.contactDetails = null;
+        this.candles = null;
+        this.deliveries = null;
+    }
+
+    private Manufacture(Builder builder) {
+        this.manufactureNumber = builder.manufactureNumber;
+        this.manufactureName = builder.manufactureName;
+        this.inventoryStock = builder.inventoryStock;
+        this.contactDetails = builder.contactDetails;
+        this.candles = builder.candles;
+        this.deliveries = builder.deliveries;
+    }
+
+    public int getManufactureNumber() {
+        return manufactureNumber;
+    }
+
+    public String getManufactureName() {
+        return manufactureName;
+    }
+
+    public String getInventoryStock() {
+        return inventoryStock;
+    }
+
+    public ContactDetails getContactDetails() {
+        return contactDetails;
+    }
+
+    public List<Candle> getCandles() {
+        return candles;
+    }
+
+    public List<Delivery> getDeliveries() {
+        return deliveries;
+    }
+
+    public void updateInventoryStock(String newStock) {
+        this.inventoryStock = newStock;
+    }
+
+    @Override
+    public String toString() {
+        return "Manufacture{" +
+                "manufactureNumber=" + manufactureNumber +
+                ", manufactureName='" + manufactureName + '\'' +
+                ", inventoryStock='" + inventoryStock + '\'' +
+                ", contactDetails=" + contactDetails +
+                ", candles=" + candles +
+                ", deliveries=" + deliveries +
+                '}';
+    }
+
+    public static class Builder {
+        private int manufactureNumber;
+        private String manufactureName;
+        private String inventoryStock;
+        private ContactDetails contactDetails;
+        private List<Candle> candles;
+        private List<Delivery> deliveries;
+
+        public Builder setManufactureNumber(int manufactureNumber) {
+            this.manufactureNumber = manufactureNumber;
+            return this;
+        }
+
+        public Builder setManufactureName(String manufactureName) {
+            this.manufactureName = manufactureName;
+            return this;
+        }
+
+        public Builder setInventoryStock(String inventoryStock) {
+            this.inventoryStock = inventoryStock;
+            return this;
+        }
+
+        public Builder setContactDetails(ContactDetails contactDetails) {
+            this.contactDetails = contactDetails;
+            return this;
+        }
+
+        public Builder setCandles(List<Candle> candles) {
+            this.candles = candles;
+            return this;
+        }
+
+        public Builder setDeliveries(List<Delivery> deliveries) {
+            this.deliveries = deliveries;
+            return this;
+        }
+
+        public Builder copy(Manufacture manufacture) {
+            this.manufactureNumber = manufacture.manufactureNumber;
+            this.manufactureName = manufacture.manufactureName;
+            this.inventoryStock = manufacture.inventoryStock;
+            this.contactDetails = manufacture.contactDetails;
+            this.candles = manufacture.candles;
+            this.deliveries = manufacture.deliveries;
+            return this;
+        }
+
+        public Manufacture build() {
+            return new Manufacture(this);
+        }
+    }
+}
