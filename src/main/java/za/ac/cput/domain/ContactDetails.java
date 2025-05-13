b75b61c7081293cc99543be8ced970aa99c3de92
XolaniMasimbe1 <222410817@mycput.ac.za>
2025-05-13 20:47:17 +0200

    Updated the code

---
+++ src/main/java/za/ac/cput/domain/ContactDetails.java
@@ -0,0 +1,15 @@
+package za.ac.cput.domain;
+
+public class ContactDetails {
+    public String email;
+    private String phoneNumber;
+    private Address address;
+
+    public ContactDetails(String email, String phoneNumber, Address address) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+}
+
